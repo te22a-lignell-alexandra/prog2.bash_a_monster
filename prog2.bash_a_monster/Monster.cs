a224079a9e8b9fc01ec4806779b193d707e6872c
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2024-09-05 16:24:02 +0200

    functioning

--- prog2.bash_a_monster/Monster.cs
+++ prog2.bash_a_monster/Monster.cs
@@ -7,6 +7,75 @@ public class Monster
     public string CurrentMood;
     public int i;
 
+    public static Monster MakeAMonster(Monster monster, string name, int hp, int strength, int mood)
+    {
+        monster.Name = name;
+        monster.Hp = hp;
+        monster.Strength = strength;
+        monster.i = mood;
+        monster.CurrentMood = monster.Mood[monster.i];
+
+        return monster;
+    }
+    
+    public static void MonsterInterraction(Monster monster, Player player)
+    {
+        while (true)
+        {
+            Console.WriteLine("choose leave, hit or feed");
+            string choice = Console.ReadLine();
+            while (choice != "feed" && choice != "hit" && choice != "leave")
+            {
+                Console.WriteLine("You must choose.");
+                choice = Console.ReadLine();
+            }
+
+            if (choice == "feed")
+            {
+                monster.i ++;
+                monster.CurrentMood = monster.Mood[monster.i];
+
+                if (monster.CurrentMood == monster.Mood[4])
+                {
+                    Console.WriteLine($"{player.Name} and {monster.Name} are friends");
+                    player.Hp += 2;
+                    Console.ReadLine();
+                    break; 
+                }
+            }
+            else if (choice == "hit")
+            {
+                if (monster.i > 0)
+                {
+                monster.i --;
+                monster.CurrentMood = monster.Mood[monster.i];
+                monster.Hp -= player.Strength;
+                }
+                if (monster.i == 0)
+                {
+                    player.Hp -= monster.Strength;
+                }
+
+                if (monster.Hp == 0)
+                {
+                    Console.WriteLine($"{monster.Name} dead");
+                    Console.ReadLine();
+                    break;
+                }
+                if (player.Hp == 0)
+                {
+                    Console.WriteLine($"{player.Name} dead");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+            else
+            {
+               break;
+            }
+        }
+    }
+
     // public Monster()
     // {
     //     CurrentMood = Mood[Random.Shared.Next(Mood.Length)];
--- prog2.bash_a_monster/Program.cs
+++ prog2.bash_a_monster/Program.cs
@@ -1,65 +1,22 @@
-﻿Player player = new Player();
+﻿using System.Runtime.ExceptionServices;
+
+Player player = new Player();
 
 Console.WriteLine("Welcome to The Game.");
 
 while (player.Name.Length < 1)
 {
-    Console.WriteLine("What is your name warrior?");
+    Console.WriteLine("What is your name random guy?");
     player.Name = Console.ReadLine();
 }
 
 Console.WriteLine($"{player.Name}...");
 
-Monster hans = new Monster() {
-    Name = "Hans" ,
-    Hp = 3 ,
-    Strength = 1
-};
+Monster hans = new Monster();
+Monster.MakeAMonster(hans, "Hans", 2, 1, 1);
+Console.WriteLine($"A monster appears. His name is {hans.Name}");
+Monster.MonsterInterraction(hans, player);
 
-hans.i = 1;
-hans.CurrentMood = hans.Mood[hans.i];
-
-while (true)
-{
-    Console.WriteLine("choose");
-    string choice = Console.ReadLine();
-    while (choice != "pet" && choice != "hit" && choice != "leave")
-    {
-        Console.WriteLine("You must choose.");
-        choice = Console.ReadLine();
-    }
-
-    if (choice == "pet")
-    {
-        hans.CurrentMood = hans.Mood[hans.i + 1];
-
-        if (hans.CurrentMood == hans.Mood[4])
-        {
-            Console.WriteLine("Best friends");
-            player.Hp += 2;
-        }
-    }
-    else if (choice == "hit")
-    {
-        hans.CurrentMood = hans.Mood[hans.i - 1];
-        hans.Hp -= player.Strength;
-
-        if (hans.CurrentMood == hans.Mood[0])
-        {
-            player.Hp -= hans.Strength;
-        }
-        if (hans.Hp == 0)
-        {
-            Console.WriteLine("Dead");
-            Console.ReadLine();
-            break;
-        }
-    }
-    else if (choice == "leave")
-    {
-        break;
-    }
-}
 
 Console.WriteLine(hans.CurrentMood);
 Console.WriteLine(hans.Hp);
